ebc212a324744dfcb451e57c847b104be2a48f85
SoA-Lee <llsa0927@naver.com>
2024-01-03 16:33:40 +0900

    Deploy : Github Action Script 작성

---
+++ src/main/java/com/eumpa/delivery/TestController.java
@@ -0,0 +1,12 @@
+package com.eumpa.delivery;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello CICD! version: ";
+    }
+}
