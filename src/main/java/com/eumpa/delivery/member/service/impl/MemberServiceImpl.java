c6375d5f15c9c1c091f8c6de3625a18fdd7f4100
백은선 <72916415+baekeunsun@users.noreply.github.com>
2024-01-05 10:09:27 +0900

    Feat : 회원가입 기능 프론트 연결 확인

---
+++ src/main/java/com/eumpa/delivery/member/service/impl/MemberServiceImpl.java
@@ -0,0 +1,39 @@
+package com.eumpa.delivery.member.service.impl;
+
+import com.eumpa.delivery.member.model.dto.JoinReqDTO;
+import com.eumpa.delivery.member.model.entity.Member;
+import com.eumpa.delivery.member.repository.MemberRepository;
+import com.eumpa.delivery.member.service.MemberService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void register(JoinReqDTO joinRequest) {
+        // 전화번호로 기존 회원을 찾음
+        Optional<Member> existingMember = memberRepository.findByPhoneNumber(joinRequest.phoneNumber());
+
+        if (existingMember.isPresent()) {
+            // 이미 회원이 존재하는 경우에 대한 처리
+            throw new IllegalArgumentException("이미 가입된 회원입니다."); // 예외 처리 또는 다른 방법으로 처리할 내용을 넣어주세요.
+        }
+
+        // 기존 회원이 없으면 새 회원으로 등록
+        Member member = Member.builder()
+                .userId(joinRequest.userId())
+                .name(joinRequest.name())
+                .phoneNumber(joinRequest.phoneNumber())
+                .build();
+
+        memberRepository.save(member);
+    }
+}
