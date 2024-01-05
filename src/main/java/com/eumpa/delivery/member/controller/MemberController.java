a41996308dad1f45a5f5f1014bb98786427d0408
백은선 <72916415+baekeunsun@users.noreply.github.com>
2024-01-05 10:13:14 +0900

    Feat : 회원가입 기능 프론트 연결 확인 (#24)
    * Deploy : Github Action S3 업로드
    
    * Deploy : Github Action S3 업로드
    
    * Deploy : Github Action S3 업로드
    
    * Feat: index 페이지
    
    * Feat : 회원가입 기능 프론트 연결 확인
    
    ---------
    
    Co-authored-by: dalgorithm <67897318+SoA-Lee@users.noreply.github.com>

---
+++ src/main/java/com/eumpa/delivery/member/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.eumpa.delivery.member.controller;
+
+import com.eumpa.delivery.member.model.dto.JoinReqDTO;
+import com.eumpa.delivery.member.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/join")
+    public String joinPage() {
+        return "join"; // Thymeleaf 템플릿 이름 (join.html)
+    }
+
+    /**
+     * 회원가입
+     * @param
+     * @return
+     * @throws
+     */
+    @PostMapping("/join")
+    @ResponseStatus(HttpStatus.OK)
+    public void join(@RequestBody JoinReqDTO joinRequest) throws Exception {
+        memberService.register(joinRequest);
+    }
+}
---
+++ src/main/java/com/eumpa/delivery/member/model/dto/JoinReqDTO.java
@@ -0,0 +1,7 @@
+package com.eumpa.delivery.member.model.dto;
+
+public record JoinReqDTO(
+    String userId,
+    String name,
+    String phoneNumber){
+}
