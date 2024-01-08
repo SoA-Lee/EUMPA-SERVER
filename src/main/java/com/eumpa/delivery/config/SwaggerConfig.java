3029d9488f6ad75bf080c4371b3ff3574768cf08
SoA-Lee <llsa0927@naver.com>
2024-01-08 14:32:07 +0900

    Docs : Swagger 설정

---
+++ src/main/java/com/eumpa/delivery/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.eumpa.delivery.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "EUMPA API 명세서",
+                description = "랩스 백엔드 스터디_EUMPA API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("배달 파트너 서비스 API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
